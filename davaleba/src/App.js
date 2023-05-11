4203b1eccc92b1d3a67e0e540b9cf26dad277b97
Ani <anilafachishvili2020@gmail.com>
2023-05-11 14:48:57 -0700

    add files

--- davaleba/src/App.js
+++ davaleba/src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
 import Lists from './Components/Notification';
-
+import Green from './images/green.png';
+import Check from './images/check.png';
+import Red from './images/red.png';
+import Error from './images/error.png';
+import Yellow from './images/yellow.png';
+import Warning from './images/warning.png';
+import Purpulee from './images/pp.png';
+import Info from './images/info.png';
 
 
 function App() {
@@ -8,23 +15,24 @@ function App() {
     <div className="App">
 
      <div className='flex'>
-      <div className='Success'>
-
-       <Lists     
-        title= "Success" 
-        text="Order Placed Successfully. You can check order delivery status."
-          />
+      <div className='Success'> 
+      <div className="line"></div>
+      <Lists     
+        title= "Success" text="Order Placed Successfully. You can check order delivery status." images={Green} green={Check} />
        </div>
-       
+
       <div className='Error'> 
-      <Lists title ="Error" text="Order Placed Successfully. You can check order delivery status."/>
+      <div className="linne"></div>
+      <Lists title ="Error" texxt="Order Placed Successfully. You can check order delivery status." red={Red} error={Error}/>
       </div>
 
       <div className='Warning'>
-       <Lists title ="Warning" text="Order Placed Successfully. You can check order delivery status."/>
+      <div className="linee"></div>
+       <Lists title ="Warning" third="Order Placed Successfully. You can check order delivery status." yellow={Yellow} warning={Warning}/>
        </div>
        <div className='Info'>
-       <Lists title ="Info" text="Order Placed Successfully. You can check order delivery status."/>
+        <div className="liine"></div>
+       <Lists title ="Info" fourth="Order Placed Successfully. You can check order delivery status." purpulle={Purpulee} inffo={Info}/>
        </div>
        </div>
     </div>
--- davaleba/src/Components/Notification.jsx
+++ davaleba/src/Components/Notification.jsx
@@ -1,20 +1,30 @@
 import "./notification.css"
 
-function Lists(props) {
+function Lists(props)  {
 
   return (
     <div>
   
       <h1 className="title">{props.title}</h1>
+      <img className="green" src={props.images} alt="" /> 
+      <img className="check" src={props.green} alt="" /> 
+      <img className="red" src={props.red} alt=""/>
+       <img className="error" src={props.error} alt="" />
+       <img className="yellow" src={props.yellow} alt="" />
+       <img className="warning" src={props.warning} alt=""/>
+       <img className="purpulle" src={props.purpulle} alt="" />
+       <img className="info" src={props.inffo} alt="" />
        <p className="text">{props.text}</p>
-     
-  
-  
-  </div>
+       <p className="texxt">{props.texxt}</p>
+       <p className="third">{props.third}</p>
+       <p className="fourth">{props.fourth}</p>
+
+       </div>
   )
 
-  
+
 };
+
 
 export default Lists;
 
